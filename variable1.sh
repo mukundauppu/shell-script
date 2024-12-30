a8934e2b2dc3691b0295e1dfd9d88427b9e94bc7
mukundakumar <mukundakumar.u@gmail.com>
2024-12-30 14:42:06 +0530

    variable added

---
+++ variable1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=mukunda
+PERSON2=keerthi
+echo "$PERSON1 :how are you"
+echo "$PERSON2:i am fine what about you"
+echo "$PERSON1 :i am fine what about you"
+echo "$PERSON2 :how are you"
