786583f7e4252b5a3c08610836cb00a11e725156
mukundakumar <mukundakumar.u@gmail.com>
2024-12-30 15:24:16 +0530

    read

---
+++ variable2.sh
@@ -0,0 +1,8 @@
+#1/bin/bash
+PERSON1=keerthi
+PERSON2=hyd
+echo "$PERSON1 going to $PERSON2"
+echo "her father isnot feeling well"
+echo "$PERSON1 : Feeling sad"
+echo "$PERSON1 :father leg lo machalu vachi"
+echo "she is going to $PERSON2 on jan "
