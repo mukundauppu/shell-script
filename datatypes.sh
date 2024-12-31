d8da013d59ccfc709329fb9ad3df9b3ac0327898
mukundakumar <mukundakumar.u@gmail.com>
2024-12-31 12:55:41 +0530

    datatypenumber

--- datatypes.sh
+++ datatypes.sh
@@ -4,6 +4,6 @@ NUMBER=$2
 TIMESTAMP=$(date)
 echo "display the date of the user :$TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
-echo "display the totalsum:$SUM"
+echo "display the totalsum:$NUMBER1 and $NUMBER2: $SUM"
 
 
