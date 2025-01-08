e142387cff9f49230b38733df684b896e8ac4c17
mukundakumar <mukundakumar.u@gmail.com>
2025-01-08 09:51:36 +0530

    jan5

--- january5shell.sh
+++ january5shell.sh
@@ -2,3 +2,6 @@
 LOG_FILE="/var/log/shellscriptlogs"
 LOG_FOLDER=$(echo $0 | cut -d "." -F1)
 echo "display $LOG_FOLDER"
+TIMESTAMP=$(date +Y -%d -%m -%s -%H)
+LOGS=$LOG_FILE/$LOG_FOLDER-$TIMESTAMP.log
+echo "$LOGS"
