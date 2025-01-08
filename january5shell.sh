7e5c561ea3006533281a67c8143f6b7716ac6a8f
mukundakumar <mukundakumar.u@gmail.com>
2025-01-08 10:05:00 +0530

    jan5

--- january5shell.sh
+++ january5shell.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+SOURCE=$1
+DEST=$2
+DAYS={3:-14}
 LOG_FILE="/var/log/shellscriptlogs"
 LOG_FOLDER=$(echo $0 | cut -d "." -f1)
 echo "display $LOG_FOLDER"
-# TIMESTAMP=$(date +Y-%m-%d-%H-%M-%S)
-# LOGS=$LOG_FILE/$LOG_FOLDER-$TIMESTAMP.log
-# echo "$LOGS"
+TIMESTAMP=$(date +Y-%m-%d-%H-%M-%S)
+LOGS=$LOG_FILE/$LOG_FOLDER-$TIMESTAMP.log
+echo "$LOGS"
+Files=$(find . -name "*.log" -mtime +14)
+USAGE()
+{
+    echo -e "January5shell.sh <SOURCE_DIR> <DES_DIR> <DAYS>"
+    exit 1
+}
+mkdir -p /home/ec2-user/shellscriptlogs
+if [ $# - lt 2 ]
+then
+echo "$USAGE"
+fi
