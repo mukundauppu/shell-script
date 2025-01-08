43b4a4e5dcb73f9347830efeea6f6b2e38da0c41
mukundakumar <mukundakumar.u@gmail.com>
2025-01-08 11:17:47 +0530

    jan5

--- january5shell.sh
+++ january5shell.sh
@@ -4,7 +4,6 @@ DEST=$2
 DAYS=${3:-14}
 LOG_FILE="/var/log/shellscriptlogs"
 LOG_FOLDER=$(echo $0 | cut -d "." -f1)
-echo "display $LOG_FOLDER"
 TIMESTAMP=$(date +Y-%m-%d-%H-%M-%S)
 LOGS=$LOG_FILE/$LOG_FOLDER-$TIMESTAMP.log
 Files=$(find $SOURCE -name "*.log" -mtime +$DAYS)
